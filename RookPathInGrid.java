1e76dc79e8d0bfd02489a1b7c47bfb2f37e7bd9a
Venkata Rama Rajesh <rama.rajesh100120@gmail.com>
2014-03-14 18:05:03 -0700

    In this edit I have used the backtracking to reduce the number of objects that are created during recursion.

--- RookPathInGrid.java
+++ RookPathInGrid.java
@@ -3,48 +3,43 @@ import java.util.Arrays;
 
 public class RookPathInGrid {
 	
-	//4x4 grid has 5x5 corners. 
+	
 	//default value initialization.
+	//This is the case for a chess board.
 	 private int cornerArrSize = 8;
 	 private int rows = 8;
 	 private int cols = 8;
 	
 	 private int getTotalRobotPaths(boolean[][] grid, int x, int y){
-		
-		boolean[][] copyGrid = deepCopy(grid);
-		
+		int total;
+
 		//making sure the indices do not cause array index of bounds exception.
 		if(x >=0 && x < rows && y>= 0 && y < cols){
-			copyGrid[x][y] = true;
+			//if the point is already traversed then it is not a valid path.	
+			if(grid[x][y]){
+				return 0;
+			}
+			grid[x][y] = true;
 		}else{
 			return 0;
 		}
-		//if the point is already traversed then it is not a valid path.
-		if(grid[x][y]){
-			return 0;
-		}
+		
 		
 		//we have reached to the bottom-right corner of grid from a path so return one.
 		if(x == (rows -1) && y == (cols-1 )){
+			//backtracking.
+			//we set the current location to false and return such
+			//that while recursing this location can be reached from another path.
+			grid[x][y] = false;
 			return 1;
 		}
-		//sum of all possible paths from top, bottom, left and right.
-		return getTotalRobotPaths(copyGrid, x+1, y) + getTotalRobotPaths(copyGrid, x, y+1) + getTotalRobotPaths(copyGrid, x-1, y) + getTotalRobotPaths(copyGrid, x, y-1);   
 		
-	}
-	 //Since the java clone doesn't deep copy and it is always advised not to use it.
-	private boolean[][] deepCopy(boolean[][] original) {
-	    if (original == null) {
-	        return null;
-	    }
-
-	    boolean[][] result = new boolean[original.length][];
-	    for (int i = 0; i < original.length; i++) {
-	        result[i] = Arrays.copyOf(original[i], original[i].length);
-	        // For Java versions prior to Java 6 use the next:
-	        // System.arraycopy(original[i], 0, result[i], 0, original[i].length);
-	    }
-	    return result;
+		//sum of all possible paths from top, bottom, left and right.
+		total = getTotalRobotPaths(grid, x+1, y) + getTotalRobotPaths(grid, x, y+1) + getTotalRobotPaths(grid, x-1, y) + getTotalRobotPaths(grid, x, y-1);
+		//backtracking.
+		grid[x][y] = false;
+		return total;
+		
 	}
 	
 	//helper function which helps users with a easy to use interface 
@@ -68,7 +63,7 @@ public class RookPathInGrid {
 	
 	public static void main(String args[]){
 		RookPathInGrid tpc = new RookPathInGrid();
-		System.out.println(tpc.getTotalRobotPaths(4,3));
+		System.out.println(tpc.getTotalRobotPaths(4,4));
 	}
 
 }
